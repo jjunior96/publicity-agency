faf8bd7b6d3106c2b0ac00cd18ff745a9dd6177c
Junior Alves <jjalves96@gmail.com>
2021-03-01 17:36:23 -0300

    chore: add new components

--- src/templates/Dashboard/index.tsx
+++ src/templates/Dashboard/index.tsx
@@ -1,11 +1,27 @@
+import Image from 'next/image';
+
+import Header from 'components/Header';
+import Footer from 'components/Footer';
 import Main from 'components/Main';
-import { Container } from './styles';
+
+import * as S from './styles';
 
 const Dashboard: React.FC = () => {
   return (
-    <Container>
-      <Main />
-    </Container>
+    <S.Container>
+      <Header />
+      <S.Content>
+        <S.ImageHome>
+          <Image src="/img/home-image.jpg" layout="fill" quality={100} />
+        </S.ImageHome>
+        <Main>
+          <S.Title>
+            Explore novas <strong>Experiências.</strong>
+          </S.Title>
+        </Main>
+      </S.Content>
+      <Footer />
+    </S.Container>
   );
 };
 
